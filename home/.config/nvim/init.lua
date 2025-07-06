ffa8b385cf485e7c2688a35759fd4c3fa23694ad
Michael Weems <mweems5@gmail.com>
2025-07-06 11:17:38 -0500

    add python lsp support

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -648,7 +648,7 @@ require('lazy').setup({
             },
           },
         },
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
